e067a03b89d4d3a2b9d443a9c4cdd48045b0f59a
Ahmed AbdELMajeed Hussein <88937645+Aabdelmajeed@users.noreply.github.com>
2024-04-27 23:54:35 +0300

    Time: 10 ms (96.83%), Space: 42.2 MB (30.93%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,32 @@
+class Foo {
+
+ public AtomicInteger first = new AtomicInteger();
+    public AtomicInteger second = new AtomicInteger();
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+           printFirst.run();
+
+        first.incrementAndGet();
+
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+         
+        while(first.get() != 1){}
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        second.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while(second.get() != 1){}
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
