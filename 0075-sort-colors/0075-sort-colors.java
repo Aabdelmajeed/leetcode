9307484f25a3facead336fc62838d5f6369bbec2
Ahmed AbdELMajeed Hussein <88937645+Aabdelmajeed@users.noreply.github.com>
2024-05-04 18:44:38 +0300

    Time: 0 ms (100.00%), Space: 42.3 MB (5.81%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        
+        int start = 0;
+        int current = 0;
+        int end = nums.length - 1;
+        while(current <= end){
+            if(nums[current] == 0){
+                if(nums[start] != 0){
+                     int temp = nums[start];
+                    nums[start] = nums[current];
+                    nums[current] = temp;
+                }
+                current++;
+                start++;
+            }else if(nums[current]==1){
+                current++;
+            }else {
+              if(nums[end] != 2){
+                     int temp = nums[end];
+                    nums[end] = nums[current];
+                    nums[current] = temp;
+                }
+                 end--;
+            }
+        }
+
+    }
+    
+    
+}
