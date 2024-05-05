23b1bd860f4f84ad58c3c4c053b2dcfb19795fd2
Ahmed AbdELMajeed Hussein <88937645+Aabdelmajeed@users.noreply.github.com>
2024-05-05 17:20:23 +0300

    Time: 22 ms (19.54%), Space: 40.9 MB (59.45%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -0,0 +1,33 @@
+class Solution {
+   public boolean circularArrayLoop(int[] nums) {
+    int n = nums.length;
+    
+    for (int i = 0; i < n; i++) {
+        int slowIdx = i;
+        int fastIdx = i;
+        boolean isForward = nums[i] > 0; 
+        
+       while(true){
+            slowIdx = getNextIndex((slowIdx + nums.length)%nums.length, nums);
+            fastIdx = getNextIndex((nums.length + getNextIndex((fastIdx + nums.length)%nums.length, nums))%nums.length, nums);
+            if(slowIdx == -1 || fastIdx == -1)
+                break;
+            if(fastIdx == slowIdx)
+                return true;
+       }
+    
+    }
+    
+    return false;  
+}
+
+private int getNextIndex(int currentIndex, int[] nums) {
+    int nextIndex = (currentIndex + nums[currentIndex]) % nums.length;
+     if (nextIndex < 0)
+        nextIndex += nums.length;
+     if (nextIndex == currentIndex || nums[nextIndex] * nums[currentIndex] <= 0)
+        return -1;
+    return nextIndex;
+}
+
+}
